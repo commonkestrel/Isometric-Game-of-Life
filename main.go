2b67f3a1a61052afa47a77c37d9a433f3fc88b95
Kestrel <87543311+commonkestrel@users.noreply.github.com>
2023-01-23 07:57:36 -0800

    Update name

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	//"github.com/faiface/pixel"
-	"github.com/Jibble330/Isometric-Game-of-Life/isometric"
+	"github.com/commonkestrel/Isometric-Game-of-Life/isometric"
 ) 
 
 func main() {
